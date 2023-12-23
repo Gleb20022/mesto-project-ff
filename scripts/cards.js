d4bb39f849a41e96d4e9c4bd61e159cf2b8d4561
Gleb Barov <glebabarov@yandex.ru>
2023-12-23 15:19:23 +0300

    add cards

--- scripts/cards.js
+++ scripts/cards.js
@@ -1,26 +1,26 @@
 const initialCards = [
     {
-      name: "Архыз",
-      link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg",
+      name: "Sea ​​turtle",
+      link: "https://plus.unsplash.com/premium_photo-1669842162407-abfc5f9e981e?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mzd8fG9jZWFuc3xlbnwwfHwwfHx8MA%3D%3D",
     },
     {
-      name: "Челябинская область",
-      link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg",
+      name: "Coral",
+      link: "https://images.unsplash.com/photo-1515555585025-54136276b6e3?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MzB8fG9jZWFuc3xlbnwwfHwwfHx8MA%3D%3D",
     },
     {
-      name: "Иваново",
-      link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg",
+      name: "Jellyfish",
+      link: "https://images.unsplash.com/photo-1495012379376-194a416fcc5f?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTUyfHxvY2VhbnN8ZW58MHx8MHx8fDA%3D",
     },
     {
-      name: "Камчатка",
-      link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg",
+      name: "Starfish",
+      link: "https://images.unsplash.com/photo-1596296415509-2234b0648407?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Nzd8fG9jZWFucyUyMFN0YXJmaXNofGVufDB8fDB8fHww",
     },
     {
-      name: "Холмогорский район",
-      link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg",
+      name: "Fish",
+      link: "https://images.unsplash.com/photo-1516683037151-9a17603a8dc7?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTAyfHxmaXNofGVufDB8fDB8fHww",
     },
     {
-      name: "Байкал",
-      link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg",
+      name: "Sea ​​urchin",
+      link: "https://images.unsplash.com/photo-1601302688238-f6f27b53daed?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NHx8c2VhJTIwdXJjaGlufGVufDB8fDB8fHww",
     }
 ];
--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,26 @@
 // @todo: Темплейт карточки
-
+const cardTemplate = document.querySelector("#card-template").content;
 // @todo: DOM узлы
-
+const placesList = document.querySelector(".places__list");
 // @todo: Функция создания карточки
+function createCard(card, deleteCard) {
+  const cardElement = cardTemplate.querySelector(".card").cloneNode(true);
+  const cardImg = cardElement.querySelector(".card__image");
 
+  cardImg.src = card.link;
+  cardImg.alt = card.name;
+  cardElement.querySelector(".card__title").textContent = card.name;
+  cardElement
+    .querySelector(".card__delete-button")
+    .addEventListener("click", deleteCard);
+
+  return cardElement;
+}
 // @todo: Функция удаления карточки
-
+function deleteCard(card) {
+  card.target.parentElement.remove();
+}
 // @todo: Вывести карточки на страницу
+initialCards.forEach((card) => {
+  placesList.append(createCard(card, deleteCard));
+});
